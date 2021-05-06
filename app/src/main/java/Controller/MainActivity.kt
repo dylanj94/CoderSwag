156916ad9c77df12bcbcb07403cd7efa25af8ef0
Dylan Jones <dylanjones500@gmail.com>
2021-05-06 07:26:06 -0600

    lesson-02

--- app/src/main/java/Controller/MainActivity.kt
+++ app/src/main/java/Controller/MainActivity.kt
@@ -1,12 +1,27 @@
 package Controller
 
+import Model.Category
+import Services.DataService
+import android.R.layout.simple_list_item_1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.dylan.coderswag.R
+import com.dylan.coderswag.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    lateinit var adapter : ArrayAdapter<Category>
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataService.categories)
+
+        binding.categoryListView.adapter = adapter
     }
 }
